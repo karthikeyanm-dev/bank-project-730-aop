4c8e652da578a155564ab69cbba247fbb8dd8ffa
karthikeyanm-dev <karthikeyan.m@itechpanda.com>
2026-06-10 08:33:35 +0530

    project created

---
+++ src/main/java/com/karthi/bank730/Bank730Application.java
@@ -0,0 +1,13 @@
+package com.karthi.bank730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank730Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bank730Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/karthi/bank730/Bank730ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthi.bank730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bank730ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
